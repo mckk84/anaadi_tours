11fb212cea3cbf729a65650ccc9844dbabecf608
mckk84 <mckk84@users.noreply.github.com>
2025-04-05 22:06:33 +0530

    login

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -90,7 +90,7 @@
 			<ul class="nav" id="pills-tab" role="tablist">
 				<li class="nav-item" role="presentation"><button class="nav-menu active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="false">Login</button></li>
 				<li class="nav-item" role="presentation"><button class="nav-menu" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="true">Register</button></li>
-				<li class="nav-item" role="presentation"><button class="nav-menu" id="pills-forgot-tab" data-bs-toggle="pill" data-bs-target="#pills-forgot" type="button" role="tab" aria-controls="pills-forgot" aria-selected="true">Forgot Password</button></li>
+				
 			</ul>
 			<div class="tab-content" id="pills-tabContent">
 				<div class="tab-pane fade active show" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
@@ -122,16 +122,7 @@
 						<p class="form-messages mb-0 mt-3"></p>
 					</form>
 				</div>
-				<div class="tab-pane fade" id="pills-forgot" role="tabpanel" aria-labelledby="pills-forgot-tab">
-					<h3 class="th-form-title mb-30">Forgot Password</h3>
-					<form action="https://html.themeholy.com/tourm/demo/mail.php" method="POST" class="login-form ajax-contact">
-						<div class="row">
-							<div class="form-group col-12"><label for="new_email">Your email*</label> <input type="text" class="form-control" name="new_email" id="new_email" required="required"></div>
-							<div class="form-btn mt-20 col-12"><button class="th-btn btn-fw th-radius2">Reset Password</button></div>
-						</div>
-						<p class="form-messages mb-0 mt-3"></p>
-					</form>
-				</div>
+				
 			</div>
 		</div>
 		
